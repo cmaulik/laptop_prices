442b5877f383bc0309097fdb523b7556fd9c1c42
cmaulik <cmaulik01@gmail.com>
2018-04-03 11:53:37 +1000

    1.0 - R version and required links

---
+++ laptop_prices.R
@@ -0,0 +1,27 @@
+# version
+# platform       x86_64-apple-darwin15.6.0   
+# arch           x86_64                      
+# os             darwin15.6.0                
+# system         x86_64, darwin15.6.0        
+# status                                     
+# major          3                           
+# minor          4.3                         
+# year           2017                        
+# month          11                          
+# day            30                          
+# svn rev        73796                       
+# language       R                           
+# version.string R version 3.4.3 (2017-11-30)
+
+# Database Access date and time 
+# [1] "2018-04-03 11:51:22 AEST" # Accessed using lubridate package
+# url : "https://www.kaggle.com/ionaskel/laptop-prices/downloads/laptops.csv/2"
+
+
+
+# Installation of following packages is required to run reproduce results.
+# install.packages("lubridate")
+
+# Loading packages
+library(lubridate)
+
